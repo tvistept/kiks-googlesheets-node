60105f9eb553b02518ac31aad5c00db846da5e6d
mvas-procont <mihail.vasiliev@procont.ru>
2025-05-20 17:30:45 +0300

    исправление ошибки KEY_FILE

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const TODAY = new Date();
 const { google } = require('googleapis');
 // const KEY_FILE = './google.json';
-const KEY_FILE = require('/app-configs/google.json');
+const {KEY_FILE} = require('/app-configs/google.json');
 const bot = new TelegramBot(token, {polling: true});
 
 async function appendRow(spreadsheetId, range, values) {
